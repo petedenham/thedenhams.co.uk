45527097e54be2622c6cd353256ab149df79db77
Peter Rattew <hello@peterrattew.co.uk>
2022-06-29 22:01:36 +0100

    feature: add header component

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -3,14 +3,14 @@ import image from "~/assets/images/logo.png";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mb-2">
-      <img src={image} width="200" height="134" className="p-1"/>
-      <nav className="text-lg">
-        <ul className="flex flex-row iterms-center px-1">
-          <li className="px-3 hover:underline"><Link to="/">Home</Link></li>
-          <li className="px-3 hover:underline"><Link to="/accommodation">Accommodation</Link></li>
-          <li className="px-3 hover:underline"><Link to="/location">Location</Link></li>
-          <li className="px-3 hover:underline"><Link to="/rsvp">RSVP</Link></li>
+    <header className="flex flex-col items-center mb-3">
+      <img src={image} className="m-2 w-60 md:w-80"/>
+      <nav className="text-base m-2">
+        <ul className="flex flex-row flex-wrap items-center text-base text-lg">
+          <li className="px-2 hover:underline"><Link to="/accommodation">Accommodation</Link></li>
+          <li className="px-2 hover:underline"><Link to="/location">Venue</Link></li>
+          <li className="px-2 hover:underline"><Link to="/rsvp">RSVP</Link></li>
+          <li className="px-2 hover:underline"><Link to="/information">Information</Link></li>
         </ul>
       </nav>
     </header>
