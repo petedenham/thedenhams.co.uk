c46724d0e0dbf79cc542aebb890e7f2ebd4ac06b
Peter Rattew <hello@peterrattew.co.uk>
2022-07-10 15:29:31 +0100

    feature: add information route

---
+++ app/routes/information.tsx
@@ -0,0 +1,16 @@
+export default function Information() {
+  return (
+    <>
+      <h1 className="text-xl my-10 font-bold">Information</h1>
+
+      <h2 className="text-lg font-bold">Children</h2>
+      <p className="mb-5">We have chosen to make this an adult only affair. We hope you understand. If you have any issues please talk to one of us directly..</p>
+
+      <h2 className="text-lg font-bold">Gifts</h2>
+      <p className="mb-5">Coming soon.</p>
+
+      <h2 className="text-lg font-bold">Dress code</h2>
+      <p className="mb-5">As long as you feel fabulous.</p>
+    </>
+  );
+}
