c8f81ff858ae997e6385b767d5ae6443559003b3
Peter Rattew <hello@peterrattew.co.uk>
2022-07-11 21:19:28 +0100

    feature: add rsvp route with coming soon message

--- app/routes/rsvp.tsx
+++ app/routes/rsvp.tsx
@@ -1,7 +1,11 @@
+import PageTitle from "~/components/page-title";
+
 export default function Rsvp() {
   return (
     <>
-      <p>RSVP</p>
+      <PageTitle>RSVP</PageTitle>
+      <p className="mb-2">Coming soon.</p>
+      <p>If you know you can't make it then please email us at <a href="mailto:wedding@thedenhams.co.uk">wedding@thedenhams.co.uk</a>.</p>
     </>
   );
 }
