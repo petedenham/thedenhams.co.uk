02b566ae0a21cc575bee3bc94419c836cedc865c
Peter Rattew <hello@peterrattew.co.uk>
2022-07-11 22:02:28 +0100

    feature: center page title on mobile

--- app/components/page-title/index.tsx
+++ app/components/page-title/index.tsx
@@ -1,5 +1,5 @@
 export default function PageTitle({ children }) {
   return (
-    <h1 className="text-xl mb-5 font-bold">{children}</h1>
+    <h1 className="text-xl mb-5 font-bold text-center lg:text-left">{children}</h1>
   );
 }
