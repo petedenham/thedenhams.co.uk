3a3233d0f1fcae2b92d045a9197c67bf712f6d07
Peter Rattew <hello@peterrattew.co.uk>
2023-01-09 18:20:13 +0000

    update snapshots

--- playwright/specs/visual/home.spec.ts
+++ playwright/specs/visual/home.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Home", () => {
   test("matches snapshot", async ({ page }) => {
-    expect(await page.screenshot()).toMatchSnapshot(["home"]);
+    await page.goto('http://localhost:3000');
+
+    expect(await page.screenshot()).toMatchSnapshot(["home.png"]);
   });
 });
