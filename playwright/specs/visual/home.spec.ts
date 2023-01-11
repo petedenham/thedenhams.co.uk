62c161c819020ad34da4e73ac62729187f56c3ab
Peter Rattew <hello@peterrattew.co.uk>
2023-01-11 11:23:38 +0000

    update visual tests

--- playwright/specs/visual/home.spec.ts
+++ playwright/specs/visual/home.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from "@playwright/test";
 test.describe("Home", () => {
   test("matches snapshot", async ({ page }) => {
     await page.goto('http://localhost:3000');
-
-    expect(await page.screenshot()).toHaveScreenshot({ path: "home.png", fullPage: true });
+    await expect(page).toHaveScreenshot({ path: "home.png", fullPage: true });
   });
 });
