8adca5f390056465568720f4486a0a5ba75c9c40
Peter Rattew <hello@peterrattew.co.uk>
2023-01-14 20:31:34 +0000

    update playwright reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,7 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [
-    ['list'],
-    ['html']
-  ],
+  reporter: process.env.CI ? 'github' : 'list',,
   use: {
     actionTimeout: 0,
     trace: 'on-first-retry',
